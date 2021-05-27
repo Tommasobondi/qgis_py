c43d4999d9e3203eb72c855ed86d771ee46b1073
Tommasobondi <80787909+Tommasobondi@users.noreply.github.com>
2021-05-27 12:00:31 +0200

    Update Start_pyton.py

--- Start_pyton.py
+++ Start_pyton.py
@@ -1,6 +1,7 @@
 #posso inserire comme ti in py, con # !!!!!!!!!
 #per saltare più righe di codici uso apici ("), uno per ongni riga sotto ad apici che voglio saltare !!!!!!!!!!!
 #word flow di pyton sempre da alto a basso
+#import -> importare librerie e funzioni da ambiente esterno
 print ("hello world")
 #per creare un programma fondamnetale assegnare valore a variabile
 #il programma non mi dara risposta come nel caso di print, prog mi legge semplicemente info e registra, 
@@ -61,13 +62,40 @@ print(c)
 #ciao3 a questo punto è il risulatato
 #parola RISERVATA, è parola con significato per pyton (ad es. print, end, or, not), le variabili non possono essere denominate con parole riservate ne con valori numerici 8ne iniziare per valori numerici)
 #nome variabile deve essere stringa che non sia parola riservata
-
-
-
-
-
-
-
+#FUNZIONI
+a= str(3)
+print(type(a))
+#print è funzione, (bhcddc) è argomento
+#per utilizzare funz mate (sin, cos...) devo imposrtare
+#import, mi permette di imposrtare da esterno in ambiente di lavoro (funzioni e librerie)
+import math
+a=math.sin(45)
+print (a)
+#Python code in one module gains access to the code in another module by the process of importing it. The import statement is the most common way of invoking the import machinery, but it is not the only way. Functions such as importlib.import_module() and built-in __import__() can also be used to invoke the import machinery.  
+#libreria math -> https://www.python.org/doc/
+#creare funzioni -> "def"
+#DEF è parola riservata che mi permette di creare funzioni
+def stampa_val #scrivo def, poi definisco nome mia funzione (stampa_val), poi devo assegnare variabili all'interno di funzione
+def stampa_val (a):
+#a questo punto py mi crea indentazione, ossia 4 righe in cui console sa che sto lavorando in funzione
+#a è paramentro, ossia variabile valida all'interno di funzione, ora vado a scrivere cosas deve fare funz in spazio indentato, nel caso voglio stapare a, quindi:
+print (a)
+#a questo punto posso eseguire mia finzione
+stampa_val (45)
+stampa_val ("ciao")
+# funzione creata esuegue ora sia per stringa che per int
+#ovviamente poi posso assegare valore a nome e poi inserisco nome in funzione
+jari= 100
+stampa_val (jari)
+#creo funzione che calcola area rettangolo, siuamilare a prima, con due variabili, gli assegno operazione e poi inserisco print, per visualizzare risultatao
+def area_rett (b,h):
+  area= b*h
+  print(area)
+#adesso ho definiyo funzione, scrivendo nome funz creata con valori mavrò risultato (FUORI DA INDENTAZIONE)
+area_rett (6,7)
+#exec(open('C:/Users/tommaso/AppData/Local/Temp/tmpu2xmbrik.py'.encode('utf-8')).read())
+#area_rett (6,7)
+#42 !!!!!!!!!!!!
 
 
 
